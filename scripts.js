c78d102ef494a88d59166896b396c8e4ed084a94
mileri <3820898@gmail.com>
2025-07-13 12:25:53 +0300

    new template

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.querySelector('.cursor');
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
-  });
